9efea01738e81bb6386c5efe11cd8c00e28bb979
harshit19424 <55681018+harshit19424@users.noreply.github.com>
2020-10-26 15:32:39 +0530

    Update Q1_part1.c

--- Q1/Q1_part1.c
+++ Q1/Q1_part1.c
@@ -1,2 +1,44 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+/* Name: Harshit Singh
+   Roll_Number: 2019424 */
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int var = 10;
+
+int main(){
+
+	
+	pid_t pid = fork();
+
+	if (pid < 0){
+		printf("Unable to create child process.\n");
+		return 1;
+	}
+	else if (pid == 0){
+		printf("In Child Process - \n\n");
+		
+		while (var > -90){
+			var--;
+			printf("Value -> %d\n", var);
+		}
+
+		printf("Value after Child Process completed -> %d\n\n", var);
+		exit(0);
+	}
+
+	else{
+		wait(0);
+		printf("In Parent Process - \n\n");
+		
+		while(var < 100){
+			var++;
+			printf("Value -> %d\n", var);
+		}
+
+		printf("Value after Parent Process completed -> %d\n\n", var);
+	}
+	return 0;
+}
